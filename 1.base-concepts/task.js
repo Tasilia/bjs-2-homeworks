d3d7d4ca3d110dbf42b7f03a5313ca5017445240
Anastasia Tsygankova <tsygnas@gmail.com>
2022-10-29 13:57:35 +0300

    Task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  "use strict";
+  const discriminant = b**2-4*a*c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr.push(-b/(2*a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-b - Math.sqrt(discriminant) )/(2*a));
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
