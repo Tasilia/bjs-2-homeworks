8b23b6b69d1a0b6221d25a5d34570892d3557b6c
Anastasia Tsygankova <tsygnas@gmail.com>
2022-11-01 20:43:59 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
+  for (let a of arr) {
+    if (a < min) {
+      min = a;
+    }
+    if (a > max) {
+      max = a;
+    }
+    sum += a;
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let a of arr) {
+    sum += a;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let arr of arrOfArr) {
+    let sum = func(arr);
+    if (sum > max) {
+      max = sum; 
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  for (let a of arr) {
+    if (a < min) {
+      min = a;
+    }
+    if (a > max) {
+      max = a;
+    }
+  }
+  return Math.abs(max - min)
 }
