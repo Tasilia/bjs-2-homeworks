80d3ae37ee3466eebc7d59be31cd6434a4d4c9c0
Anastasia Tsygankova <tsygnas@gmail.com>
2022-11-04 14:15:11 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);
 }
