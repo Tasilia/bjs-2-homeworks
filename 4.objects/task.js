8df4ad17cac4d231fe12616e4fe63452ba813753
Anastasia Tsygankova <tsygnas@gmail.com>
2022-11-06 17:25:43 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+}
+Student.prototype.addMark = addMark;
+Student.prototype.addMarks = addMarks;
+Student.prototype.getAverage = getAverage;
+Student.prototype.exclude = exclude;
+
+function addMark(mark) {
+  if(this.marks === undefined) { 
+    this.marks = [mark];
+  } else {
+      this.marks.push(mark);
+  }
 }
 
-// ваш код для остальных методов
+function addMarks(...args) {
+  for (let arg of args) {
+    this.addMark(arg);
+  }
+}
+
+function getAverage() {
+  return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+}
+
+function exclude(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
