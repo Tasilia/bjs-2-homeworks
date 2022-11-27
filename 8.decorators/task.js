17bf344af5baae3ec8fc9908719acf8c9f26e759
Tasilia <tsygnas@gmail.com>
2022-11-27 19:00:36 +0300

    initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+
+  function wrapper(...args) {
+      const hash = args.join(","); // получаем правильный хэш
+      let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+      if (objectInCache) { // если элемент найден
+          console.log("Из кэша: " + objectInCache.value);
+          return "Из кэша: " + objectInCache.value;
+      }
+
+      let result = func(...args); // в кэше результата нет - придётся считать
+      cache.push({hash: hash, value: result}) ; // добавляем элемент с правильной структурой
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+  }
+  return wrapper;
+  }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  let count = 0;
+  let allCount = 0;
+  let isActive = true;
+  function wrapper(...args) {
+    wrapper.allCount = allCount++;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    if(isActive) {
+      isActive = false;
+      wrapper.count = count++;
+      func(...args);
+    }
+    timeoutId = setTimeout(() => {
+      timeoutId = null;
+      wrapper.count = count++;
+      func(...args);
+    }, delay);
+  }
+  return wrapper;
 }
